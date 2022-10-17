20ce0c582234f43650f1d6b756e6919fd67fb7d4
Peter Thoman <peter@metaclassofnil.com>
2022-10-17 16:01:58 +0200

    Use const in example 02_11

--- 02_classes_and_interfaces/02_11_hidden_friends.cpp
+++ 02_classes_and_interfaces/02_11_hidden_friends.cpp
@@ -10,21 +10,35 @@ class Bounds {
   private:
         int min, max;
 
-        friend std::ostream& operator<<(std::ostream& os, Bounds& b);
+        friend std::ostream& operator<<(std::ostream& os, const Bounds& b);
 };
 
-std::ostream& operator<<(std::ostream& os, Bounds& b) {
+std::ostream& operator<<(std::ostream& os, const Bounds& b) {
         return os << "[" << b.min << "," << b.max << "]";
 }
+
+class BoundsHidden {
+  public:
+	BoundsHidden(int min, int max) : min(min), max(max) {}
+
+  private:
+	int min, max;
+
+	friend std::ostream& operator<<(std::ostream& os, const BoundsHidden& b) {
+		return os << "[" << b.min << "," << b.max << "]";
+	}
+};
 
 class Foo {
 };
 
 int main() {
         Bounds b{1,5};
-
         std::cout << b << std::endl;
 
-        Foo f;
-        std::cout << f << std::endl;
+		BoundsHidden bh{2,6};
+		std::cout << bh << std::endl;
+
+        //Foo f;
+        //std::cout << f << std::endl;
 }
